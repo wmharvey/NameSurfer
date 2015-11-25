583985f3597302772471ed9ee6c3ce16e7124e16
S <s>
2015-11-25 06:40:21 -0800

    {"type":"TI", "runs":"1"}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -64,8 +64,8 @@ public class NameSurferGraph extends GCanvas
 			add(new GLine(i * (getWidth() / NDECADES), 0, i * (getWidth() / NDECADES), getHeight()) );
 		}
 		for (int i = 0; i < NDECADES; i++) {
-			GLabel label = new GLabel("" + (START_DECADE + (5 * i)));
-			double x = 10 + ((getWidth() / NDECADES) * i);
+			GLabel label = new GLabel("" + (START_DECADE + (10 * i)));
+			double x = 5 + ((getWidth() / NDECADES) * i);
 			double y = getHeight() - ((GRAPH_MARGIN_SIZE - label.getAscent()) / 2);
 			add(label, x, y);
 		}
