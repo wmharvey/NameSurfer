22fdd85eea8f8c9d8c81b045b3d0ae2d993d014d
S <s>
2015-11-25 05:43:03 -0800

    {"type":"TI", "runs":"0"}

--- NameSurferGraph.java
+++ NameSurferGraph.java
@@ -24,7 +24,11 @@ public class NameSurferGraph extends GCanvas
 	}
 	
 	private void createBackground() {
-		
+		GLine topline = new GLine(0, GRAPH_MARGIN_SIZE, getWidth(), GRAPH_MARGIN_SIZE);
+		GLine bottomline = new GLine(0, getHeight() - GRAPH_MARGIN_SIZE, 
+									getWidth(), getHeight() - GRAPH_MARGIN_SIZE);
+		add(topline);
+		add(bottomline);
 	}
 	
 	
