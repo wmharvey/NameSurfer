853a700ad757b9e4aab64cf540dbc5394db0d25d
S <s>
2015-11-25 04:47:22 -0800

    {"type":"TI", "runs":"1"}

--- NameSurfer.java
+++ NameSurfer.java
@@ -22,12 +22,6 @@ public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
 	    initControlBar();
 	}
 	
-	public void run() {
-		NameSurferEntry entry = new NameSurferEntry("Sam 0 69 99 131 168 236 278 380 1000 405 566");
-		println(entry.getName());
-		println(entry.getRank(3));
-		println(entry);
-	}
 
 /* Method: actionPerformed(e) */
 /**
